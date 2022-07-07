97e098e21846341909b3f4c33ca62ae1dc0632f8
Wang Boyu <boyu.wby@gmail.com>
2022-07-07 20:49:47 +0800

    use Slider instead of UserSettableParameter

--- scripts/run.py
+++ scripts/run.py
@@ -1,5 +1,6 @@
 import argparse
 
+import mesa
 from mesa.visualization.UserParam import UserSettableParameter
 from mesa_geo.visualization.ModularVisualization import ModularServer
 from mesa_geo.visualization.modules import MapModule
@@ -41,10 +42,10 @@ if __name__ == "__main__":
         "show_walkway": True,
         "show_lakes_and_rivers": True,
         "show_driveway": True,
-        "num_commuters": UserSettableParameter('slider', 'Number of Commuters',
-                                               value=50, min_value=10, max_value=150, step=10),
-        "commuter_speed": UserSettableParameter('slider', 'Commuter Walking Speed (m/s)',
-                                                value=1.0, min_value=0.1, max_value=1.5, step=0.1)
+        "num_commuters": mesa.visualization.Slider('Number of Commuters',
+                                                   value=50, min_value=10, max_value=150, step=10),
+        "commuter_speed": mesa.visualization.Slider('Commuter Walking Speed (m/s)',
+                                                    value=1.0, min_value=0.1, max_value=1.5, step=0.1)
     }
     map_params = {
         "ub": {"view": [43.0022471679366, -78.785149],
